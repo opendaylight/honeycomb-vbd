074a6533cc321d6ecb3df5f40c50c38e92d46ea7
Tomas Cechvala <tcechval@cisco.com>
2017-03-15 10:24:22 +0100

    Bug 8009 - adding role to physical interfaces
    VBD takes first read interface that matches certain conditions as a
    carrier interface for VXLAN tunnels. This lead to mismatched tunnel
    endpoint addresses in case multiple physical interfaces match criteria.
    
    E.g. NodeA:GE1,GE2 and NodeB:GE1,GE2 can lead to 6 different src and
    dst tunnel combinations.
    
    This is resolved by specifying which physical interfaces are involved in
    topology processing. The configuration can be defined by a config file
    and seen in runtime in operational DS.
    
    Change-Id: Iee539d3b648f7352533b4d825f9eb55845dbbf40
    Signed-off-by: Tomas Cechvala <tcechval@cisco.com>

--- impl/src/main/java/org/opendaylight/vbd/impl/VirtualBridgeDomainManager.java
+++ impl/src/main/java/org/opendaylight/vbd/impl/VirtualBridgeDomainManager.java
@@ -8,21 +8,42 @@
 
 package org.opendaylight.vbd.impl;
 
-import com.google.common.base.Preconditions;
+import java.util.List;
+import java.util.concurrent.Executors;
+
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.MountPointService;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.TerminationPointVbridgeCfgAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.TerminationPointVbridgeCfgAugmentBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.TopologyTypesVbridgeAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.TopologyVbridgeCfgAugment;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.TopologyVbridgeCfgAugmentBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.network.topology.topology.VbridgeStartupConfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.network.topology.topology.node.termination.point._interface.type.cfg.VirtualDomainCarrierCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.network.topology.topology.node.termination.point._interface.type.cfg.virtual.domain.carrier._case.VirtualDomainCarrierBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.vbridge.topology.rev160129.network.topology.topology.topology.types.VbridgeTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.TopologyTypes;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 /**
  * Tip for the Virtual Bridge Domain implementation. This class is instantiated when the application is started
@@ -35,18 +56,73 @@ public final class VirtualBridgeDomainManager implements AutoCloseable {
                     InstanceIdentifier.builder(NetworkTopology.class).child(Topology.class).child(TopologyTypes.class)
                     .augmentation(TopologyTypesVbridgeAugment.class).child(VbridgeTopology.class).build());
 
+    private static final String UNSPECIFIED_INIT_INTF_CONFIG = "unspecified";
+    private static final int WRITE_RETRY_TIMEOUT = 600;
+    private static final int NUM_OF_WRITE_RETRIES = 15;
     private final ListenerRegistration<TopologyMonitor> reg;
+
     private boolean closed;
 
     private VirtualBridgeDomainManager(final ListenerRegistration<TopologyMonitor> reg) {
         this.reg = Preconditions.checkNotNull(reg);
     }
 
-    public static VirtualBridgeDomainManager create(@Nonnull final DataBroker dataBroker,@Nonnull MountPointService mountService) {
+    public static VirtualBridgeDomainManager create(@Nonnull final DataBroker dataBroker,
+            @Nonnull MountPointService mountService, @Nullable String virtualDomainInterfaces) {
         final ListenerRegistration<TopologyMonitor> reg =
                 dataBroker.registerDataTreeChangeListener(LISTEN_TREE, new TopologyMonitor(dataBroker, mountService));
-
+        if (!Strings.isNullOrEmpty(virtualDomainInterfaces)
+                && !virtualDomainInterfaces.equals(UNSPECIFIED_INIT_INTF_CONFIG)) {
+            Executors.newSingleThreadExecutor().submit(() -> {
+                for (int i = 0; i < NUM_OF_WRITE_RETRIES; i++) {
+                    WriteTransaction wTx = dataBroker.newWriteOnlyTransaction();
+                    try {
+                        Thread.sleep(WRITE_RETRY_TIMEOUT);
+                        writeStartupConfig(wTx, virtualDomainInterfaces);
+                        wTx.submit().get();
+                        break;
+                    } catch (Exception e) {
+                        LOG.warn("Failed to write startup data to operational DS. Attempt num {}, {} left.", i,
+                            NUM_OF_WRITE_RETRIES - 1 - i);
+                        try {
+                            wTx.cancel();
+                        } finally {
+                            wTx = null;
+                        }
+                    }
+                }
+            });
+        }
         return new VirtualBridgeDomainManager(reg);
+    }
+
+    public static void writeStartupConfig(@Nonnull WriteTransaction wTx, @Nonnull String virtualDomainInterfaces) {
+        Topology confTopology = new TopologyBuilder().setTopologyId(VbdUtil.STARTUP_CONFIG_TOPOLOGY)
+            .addAugmentation(TopologyVbridgeCfgAugment.class, getTopologyAug().build())
+            .build();
+        wTx.put(LogicalDatastoreType.OPERATIONAL, VbdUtil.topologyIid(VbdUtil.STARTUP_CONFIG_TOPOLOGY).build(),
+                confTopology, true);
+        for (String intfOnNode : Sets.newConcurrentHashSet(Splitter.on(",").split(virtualDomainInterfaces))) {
+            List<String> entries = Lists.newArrayList(Splitter.on(":").split(intfOnNode));
+            if (entries.size() != 2) {
+                LOG.warn("Cannot resolve {} initial configuration for interface.", intfOnNode);
+                continue;
+            }
+            NodeId nodeId = new NodeId(entries.get(0));
+            TpId tpId = new TpId(entries.get(1));
+            LOG.info("Writing startup config for interface " + tpId + " at " + nodeId);
+            wTx.put(LogicalDatastoreType.OPERATIONAL,
+                    VbdUtil.terminationPointIid(VbdUtil.STARTUP_CONFIG_TOPOLOGY, nodeId, tpId)
+                        .augmentation(TerminationPointVbridgeCfgAugment.class).build(), getTpAug().build(), true);
+        }
+    }
+
+    private static TerminationPointVbridgeCfgAugmentBuilder getTpAug() {
+        return new TerminationPointVbridgeCfgAugmentBuilder().setInterfaceTypeCfg(new VirtualDomainCarrierCaseBuilder().build());
+    }
+
+    private static TopologyVbridgeCfgAugmentBuilder getTopologyAug() {
+        return new TopologyVbridgeCfgAugmentBuilder().setVbridgeStartupConfig(new VbridgeStartupConfigBuilder().build());
     }
 
     @Override
